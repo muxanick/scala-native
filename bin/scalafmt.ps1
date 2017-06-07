246a54de8f907961074f5817a92c90e7d3353188
MuxaHuk <muxanick@gmail.com>
2017-06-07 19:27:41 +0100

    back

--- bin/scalafmt.ps1
+++ bin/scalafmt.ps1
@@ -10,8 +10,7 @@ param (
     [string]$testMode
 )
 
-$SCALAFMT_VERSION="1.0.0-RC1"
-#"0.6.8"
+$SCALAFMT_VERSION="0.6.8"
 $SN = "$env:USERPROFILE\.scala-native"
 $SCALAFMT="$SN\.scalafmt-$SCALAFMT_VERSION.jar"
 $COURSIER="$PSScriptRoot\coursier.ps1"
